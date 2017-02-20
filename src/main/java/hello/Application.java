836fbe45cdfc089f7dce90e238fa9fe9f70db94f
robert.kuhar <robert.kuhar@climate.com>
2017-02-20 12:14:46 -0800

    Implemented the whole of the tutorial.  Its kind-of lame.  No tests?!?

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,15 +1,33 @@
 package hello;
 
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
   private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
     LOG.info("main(args: {})", Arrays.toString(args));
-    System.out.println("hello");
+
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  CommandLineRunner init(StorageService storageService) {
+    return (args) -> {
+      storageService.deleteAll();
+      storageService.init();
+    };
   }
 }
