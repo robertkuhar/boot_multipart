30aecc9ab0853936437ebbfa9a370b281f3d734c
robert.kuhar <robert.kuhar@climate.com>
2017-02-20 11:43:33 -0800

    Add logback

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,15 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
 public class Application {
-    public static void main(String[] args) {
-	System.out.println("hello");
-    }
+  private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    LOG.info("main(args: {})", Arrays.toString(args));
+    System.out.println("hello");
+  }
 }
